c14182dbfda834087ef0d2f5cc751409f61bbcd1
Alan Busby <abusby@gracenote.com>
2013-09-06 16:48:44 +0900

    Adding files to repo

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject easy-parse "1.4.3"
+  :description "Library to simplify the parsing of Documents, especially XML"
+  :url         "https://github.com/thebusby/easy-parse"
+  :license     {:name "MIT"
+                :url "http://opensource.org/licenses/MIT"}
+  :dependencies [[org.clojure/clojure "1.4.0"]])
---
+++ src/easy_parse.clj
@@ -0,0 +1,25 @@
+(ns easy-parse
+  "A simple library to assist with parsing Documents, especially XML."
+  )
+
+(declare easy-parse)
+
+(defprotocol ParseElems
+  (parse-element [expr tree]))
+(extend-protocol ParseElems
+
+  clojure.lang.IFn
+  (parse-element [query tree]
+    (query tree))
+
+  clojure.lang.PersistentVector
+  (parse-element [[query children] tree]
+    (mapv #(easy-parse %1 children)
+          (query tree))))
+
+(defn easy-parse [tree expr]
+  "Parses a tree, and returns the requested fields into the provided map structure."
+  (->> expr
+       (mapv (fn [[k v]]
+               (vector k (parse-element v tree))))
+       (into {})))
